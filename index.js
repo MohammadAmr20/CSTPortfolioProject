4e361a3413e57dee0e55ab372d1fe4001d632565
MohammadAmr20 <midoamr20@gmail.com>
2023-08-25 16:41:15 +0300

    ProjectsSection

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const menu = document.querySelector("header .container nav .toggle-menu + ul");
 const header = document.querySelector("header");
 const navanchor = document.querySelectorAll("header .container nav ul a");
 const sections = document.querySelectorAll(".sections");
+
+const selectors = document.querySelectorAll('.shuffle li');
+
+const projects = document.querySelectorAll('.project-box');
+
+console.log(projects[0].classList[1]);
+
 console.log(toggleMenu);
 toggleMenu.addEventListener("click", () => {
   if (toggleMenu.style.display != "none") {
@@ -36,3 +43,23 @@ window.onscroll = () => {
     }
   }
 };
+
+const unClickSelectors = ()=>{
+  for(let selector of selectors)
+    selector.classList.remove('shuffle-active');
+}
+
+
+for(let selector of selectors){
+  selector.addEventListener("click" , ()=>{
+    unClickSelectors();
+    selector.classList.add('shuffle-active');
+    let containsAll = selector.classList.contains('all');
+    for(let project of projects){
+      if(project.classList[1] === selector.classList[0] || containsAll)
+        project.classList.remove('hide');
+      else
+        project.classList.add('hide');
+    }
+  });
+}
